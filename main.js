c6c1c49304dc9f802bb5c0cf892e54c340861edf
Mohamed Abo Hebil <mabuhebil@gmail.com>
2025-06-22 19:00:10 +0300

    final update

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const generateBtn = document.querySelector('button')
 const minInput = document.querySelector('.min')
 const maxInput = document.querySelector('.max')
+const resultDiv = document.querySelector('.result')
 
 
 console.log(generateBtn)
@@ -8,7 +9,17 @@ function Random() {
     let minValue = Number(minInput.value)
     let maxValue = Number(maxInput.value)
 
-    console.log(minValue, maxValue)
+    if (minValue > maxValue) {
+
+        minValue = minValue + maxValue;
+        maxValue = minValue - maxValue;
+        minValue = minValue - maxValue;
+        minInput.value = minValue;
+        maxInput.value = maxValue;
+    }
+
+    let num = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+    resultDiv.textContent = num
 }
 
 
